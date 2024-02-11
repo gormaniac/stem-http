2e3f4cdb1f7077ce45c303668e0b1854fd0a5365
gormaniac <83027042+gormaniac@users.noreply.github.com>
2024-02-11 08:06:03 -0700

    docs: update ProxyMgr docstrings

--- src/stem_http/tor.py
+++ src/stem_http/tor.py
@@ -19,6 +19,33 @@ class ProxyMgr:
 
     TODO - Support country changes
     TODO - Expose a method to open raw sockets through the proxy.
+
+    Parameters
+    ----------
+    reuse : bool, optional
+        Reuse an existing Tor proxy, by default ``False``.
+    retry : bool, optional
+        Try starting a managed Tor process multiple times until one can start,
+        by default ``True``.
+    countries : Optional[list], optional
+        A list of countries to use as exit nodes, by default ``None``.
+    passw : Optional[str], optional
+        An optional password to authenticate to the Tor proxy's control port,
+        by default ``None``.
+    socks_port : int, optional
+        The port of the Tor SOCKS proxy to connect to or use in managed processes. Retries
+        will change this port as needed until a valid port is found. By default ``9050``. 
+    cntrl_port : int, optional
+        The Tor proxy's control port to connect to, by default ``9051``.
+    exit_on_reuse_fail : bool, optional
+        Raise an exception if reusing an existing proxy fails. If ``False``, the object
+        tries to start a managed process on the specified ports when reuse fails.
+        By default ``False``.
+
+    Raises
+    ------
+    RuntimeError
+        When reusing a proxy fails and ``exit_on_reuse_fail`` is ``True``.
     """
 
     def __init__(
@@ -50,6 +77,7 @@ class ProxyMgr:
                     raise RuntimeError("No existing Tor proxy to connect to.") from err
                 self.reuse = False
 
+        # This is not an `else` because an error above can set self.reuse to False.
         if not self.reuse:
             self.start()
             self.connect_cntrlr()

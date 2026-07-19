f5ab6ff6dfa1a4aebf30e2cd326c14cb2dbdf074
gormaniac <83027042+gormaniac@users.noreply.github.com>
2026-07-18 23:37:43 -0600

    docs: switch to the shibuya theme

--- doc/conf.py
+++ doc/conf.py
@@ -6,11 +6,42 @@ project = "stem_http"
 copyright = "2024, John Gorman"
 author = "John Gorman"
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.githubpages"]
+extensions = ["sphinx.ext.autodoc"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
+html_theme = "shibuya"
+html_static_path = ["_static"]
+html_title = "stem_http Docs"
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = []
+html_baseurl = 'https://docs.gormo.co/stem-http/'
+
+html_theme_options = {
+    "accent_color": "teal",
+    "color_mode": "auto",
+    "nav_socials": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/gormaniac/stem-http",
+            "icon": "simple-icons:github",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/stem-http/",
+            "icon": "simple-icons:pypi",
+        },
+    ],
+    "foot_socials": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/gormaniac/stem-http",
+            "icon": "simple-icons:github",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/stem-http/",
+            "icon": "simple-icons:pypi",
+        },
+    ]
+}
--- doc/stem_http.rst
+++ doc/stem_http.rst
@@ -9,21 +9,21 @@ stem\_http.client module
 
 .. automodule:: stem_http.client
    :members:
-   :undoc-members:
    :show-inheritance:
+   :undoc-members:
 
 stem\_http.tor module
 ---------------------
 
 .. automodule:: stem_http.tor
    :members:
-   :undoc-members:
    :show-inheritance:
+   :undoc-members:
 
 Module contents
 ---------------
 
 .. automodule:: stem_http
    :members:
-   :undoc-members:
    :show-inheritance:
+   :undoc-members:
